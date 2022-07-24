64eee3d64e3e5719dc8a74bd5221469bb02a2ad9
n3tsploit <netsploit@tutanota.com>
2022-07-24 16:33:02 +0300

    debugging the functions

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 ./telebot/app.py
+web: python ./telebot/app.py
--- telebot/app.py
+++ telebot/app.py
@@ -144,7 +144,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN,
-                          webhook_url="https://powerinterruption.herokuapp.com/" + TOKEN)
+                          webhook_url=f"https://powerinterruption.herokuapp.com/{TOKEN}" )
     updater.idle()
 
 
