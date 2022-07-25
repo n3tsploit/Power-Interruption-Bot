537063adf0a2a7027198a6963274fc64086cb67b
n3tsploit <netsploit@tutanota.com>
2022-07-25 10:06:21 +0300

    debugging the functions

--- telebot/app.py
+++ telebot/app.py
@@ -1,152 +1,66 @@
-from telegram.ext import *
-from telegram import *
-from telebot import functions
-from dotenv import load_dotenv
-from pathlib import Path
+"""
+Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
+Deployed using heroku.
+Author: liuhh02 https://medium.com/@liuhh02
+"""
+
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
+PORT = int(os.environ.get('PORT', 5000))
 
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-load_dotenv(Path(".env"))
-TOKEN = os.getenv('bot_token')
-URL = os.getenv('URL')
-PORT = int(os.environ.get('PORT', '443'))
+logger = logging.getLogger(__name__)
+TOKEN = os.getenv('TOKEN')
 
-print('Bot is starting')
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-County, Area, Place = range(3)
-global county_value
-global area_value
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
-def start_command(update, context):
-    update.message.reply_text('Welcome')
-    inline_keyboard = [[InlineKeyboardButton(text="Baringo", callback_data="Baringo"),
-                        InlineKeyboardButton(text="Bomet", callback_data="Bomet"),
-                        InlineKeyboardButton(text="Bungoma", callback_data="Bungoma")],
-                       [InlineKeyboardButton(text="Busia", callback_data="Busia"),
-                        InlineKeyboardButton(text="Elgeyo-Marakwet", callback_data="Elgeyo-Marakwet"),
-                        InlineKeyboardButton(text="Embu", callback_data="Embu")], [
-                           InlineKeyboardButton(text="Garissa", callback_data="Garissa"),
-                           InlineKeyboardButton(text="HomaBay", callback_data="HomaBay"),
-                           InlineKeyboardButton(text="Isiolo", callback_data="Isiolo")], [
-                           InlineKeyboardButton(text="Kajiado", callback_data="Kajiado"),
-                           InlineKeyboardButton(text="Kakamega", callback_data="Kakamega"),
-                           InlineKeyboardButton(text="Kericho", callback_data="Kericho")], [
-                           InlineKeyboardButton(text="Kiambu", callback_data="Kiambu"),
-                           InlineKeyboardButton(text="Kilifi", callback_data="Kilifi"),
-                           InlineKeyboardButton(text="Kirinyaga", callback_data="Kirinyaga")], [
-                           InlineKeyboardButton(text="Kisii", callback_data="Kisii"),
-                           InlineKeyboardButton(text="Kisumu", callback_data="Kisumu"),
-                           InlineKeyboardButton(text="Kitui", callback_data="Kitui")], [
-                           InlineKeyboardButton(text="Kwale", callback_data="Kwale"),
-                           InlineKeyboardButton(text="Laikipia", callback_data="Laikipia"),
-                           InlineKeyboardButton(text="Lamu", callback_data="Lamu")], [
-                           InlineKeyboardButton(text="Machakos", callback_data="Machakos"),
-                           InlineKeyboardButton(text="Makueni", callback_data="Makueni"),
-                           InlineKeyboardButton(text="Mandera", callback_data="Mandera")], [
-                           InlineKeyboardButton(text="Marsabit", callback_data="Marsabit"),
-                           InlineKeyboardButton(text="Meru", callback_data="Meru"),
-                           InlineKeyboardButton(text="Migori", callback_data="Migori")], [
-                           InlineKeyboardButton(text="Mombasa", callback_data="Mombasa"),
-                           InlineKeyboardButton(text="Murang’a", callback_data="Murang’a"),
-                           InlineKeyboardButton(text="Nairobi", callback_data="Nairobi")], [
-                           InlineKeyboardButton(text="Nakuru", callback_data="Nakuru"),
-                           InlineKeyboardButton(text="Nandi", callback_data="Nandi"),
-                           InlineKeyboardButton(text="Narok", callback_data="Narok")], [
-                           InlineKeyboardButton(text="Nyamira", callback_data="Nyamira"),
-                           InlineKeyboardButton(text="Nyandarua", callback_data="Nyandarua"),
-                           InlineKeyboardButton(text="Nyeri", callback_data="Nyeri")], [
-                           InlineKeyboardButton(text="Samburu", callback_data="Samburu"),
-                           InlineKeyboardButton(text="Siaya", callback_data="Siaya"),
-                           InlineKeyboardButton(text="Taita-Taveta", callback_data="Taita-Taveta")], [
-                           InlineKeyboardButton(text="TanaRiver", callback_data="TanaRiver"),
-                           InlineKeyboardButton(text="Tharaka-Nithi", callback_data="Tharaka-Nithi"),
-                           InlineKeyboardButton(text="TransNzoia", callback_data="TransNzoia")], [
-                           InlineKeyboardButton(text="Turkana", callback_data="Turkana"),
-                           InlineKeyboardButton(text="UasinGishu", callback_data="UasinGishu"),
-                           InlineKeyboardButton(text="Vihiga", callback_data="Vihiga")], [
-                           InlineKeyboardButton(text="Wajir", callback_data="Wajir"),
-                           InlineKeyboardButton(text="WestPokot", callback_data="WestPokot")]]
-
-    reply_keyboard_markup = InlineKeyboardMarkup(inline_keyboard, one_time_keyboard=True, resize_keyboard=True)
-    update.message.reply_text("Choose a  You want to check power interruption details:    ",
-                              reply_markup=reply_keyboard_markup)
-    return Area
-
-
-def area(update, context):
-    query = update.callback_query
-    query.answer()
-    global county_value
-    county_value = query.data
-    data = functions.area_list(county_value)
-    if data is None:
-        query.edit_message_text('Yaay..There Isn\'t any power interruption scheduled for this area.\n Bye')
-        return ConversationHandler.END
-    inline_keyboard = []
-    for i in data:
-        inline_keyboard.append(InlineKeyboardButton(text=i, callback_data=i))
-
-    print(inline_keyboard)
-    reply_keyboard_markup = InlineKeyboardMarkup([inline_keyboard[i:i + 2] for i in range(0, len(inline_keyboard), 2)])
-    query.edit_message_text('--fetching--')
-    query.edit_message_text(text="Choose a Area ", reply_markup=reply_keyboard_markup)
-    return Place
-
-
-def place(update, context):
-    query = update.callback_query
-    query.answer()
-    area_value = query.data
-    place_value, time_value = functions.place_list(area=query.data, county=county_value)
-    place_value = '\n'.join(place_value)
-    place_value = time_value + '\n' + '-' * 55 + '\n' + place_value
-    query.edit_message_text('--fetching--')
-    query.edit_message_text(place_value)
-
-    context.bot.sendMessage(text='Bye', chat_id=update.effective_chat.id)
-    return ConversationHandler.END
-
-
-def help_command(update, context):
-    update.message.reply_text('These are some of the commands')
-
-
-def stop(update, context):
-    update.message.reply_text('Bye..see you later')
-
-    return ConversationHandler.END
-
-
-def unknown(update, context):
-    update.message.reply_text('Sorry I cannot understand the text!')
-
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
-    disp = updater.dispatcher
 
-    disp.add_handler(CommandHandler('help', help_command))
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start_command)],
-        states={
-            Area: [CallbackQueryHandler(area)],
-            Place: [CallbackQueryHandler(place)]
-        },
-        fallbacks=[CommandHandler('stop', stop)]
-    )
-    disp.add_handler(conv_handler)
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
-    disp.add_handler(MessageHandler(Filters.text,unknown))
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
-                          port=443,
-                          url_path=TOKEN,
-                          webhook_url='https://powerinterruption.herokuapp.com/' + TOKEN)
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://powerinterruption.herokuapp.com/' + TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
-
-if __name__ == '__main__':
-    main()
