16e95efdaa1b1668ecf8b5ded289baa078ea93f1
n3tsploit <netsploit@tutanota.com>
2022-07-25 10:20:12 +0300

    debugging the functions

--- telebot/app.py
+++ telebot/app.py
@@ -57,8 +57,8 @@ def main():
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://powerinterruption.herokuapp.com/' + TOKEN)
+                          url_path=TOKEN,
+                          webhook_url='https://powerinterruption.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
